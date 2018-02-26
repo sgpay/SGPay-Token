bec054e71f6df2b10b4a275d7e8879a8610921c9
Nidish Ramakrishnan <nramakrishnan@grepruby.com>
2018-02-26 17:30:11 +0530

    Cleanup to test cases

--- contracts/SGPay/SGPayPresale.sol
+++ contracts/SGPay/SGPayPresale.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.11;
 
-import '../crowdsale/singlestage/TokenCappedCrowdsale.sol';
+import '../crowdsale/singlestage/TokenCappedCrowdsalePre.sol';
 
 
 /**
@@ -9,7 +9,7 @@ import '../crowdsale/singlestage/TokenCappedCrowdsale.sol';
  * The way to add new features to a base crowdsale is by multiple inheritance.
  * In order to switch between multistage and single stage, one must also change base contract import of the add-ons.
  * In this example we are providing following extensions:
- * TokenCappedCrowdsale - sets a max boundary for Token sold in milestones
+ * TokenCappedCrowdsalePre - sets a max boundary for Token sold in milestones
  * RefundableCrowdsale - set a min goal to be reached and returns funds if it's not met
  * After adding multiple features it's good practice to run integration tests
  * to ensure that subcontracts works together as intended.
@@ -20,12 +20,12 @@ import '../crowdsale/singlestage/TokenCappedCrowdsale.sol';
  // _rate - 2125
  // _cap - 2000000e18
 
-contract SGPayPresale is TokenCappedCrowdsale {
+contract SGPayPresale is TokenCappedCrowdsalePre {
 
 
   function SGPayPresale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet, address controller, uint256 _cap)
-    Crowdsale(_startTime, _endTime, _rate, _wallet, controller)
-    TokenCappedCrowdsale(_cap)
+    CrowdsalePre(_startTime, _endTime, _rate, _wallet, controller)
+    TokenCappedCrowdsalePre(_cap)
   {
     // require(_cap.div(rate) > _goal);
   }
